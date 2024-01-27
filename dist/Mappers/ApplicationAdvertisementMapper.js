457ac782c76719fee6705f7a575824f51b5373af
Zaw Nay Lin <zawnaylin.franz@gmail.com>
2024-01-27 20:18:48 +0630

    Fix: Change Typescript compile target to ESNext

--- dist/Mappers/ApplicationAdvertisementMapper.js
+++ dist/Mappers/ApplicationAdvertisementMapper.js
@@ -1,9 +1,8 @@
 export const applicationAdvertisementMapper = (src) => {
-    var _a;
     return {
         body: src.body,
         appStoreLink: src.appStoreLink,
-        featureImage: (_a = src.featureImage) === null || _a === void 0 ? void 0 : _a.url,
+        featureImage: src.featureImage?.url,
         title: src.title,
         googlePlayLink: src.googlePlayLink,
     };
